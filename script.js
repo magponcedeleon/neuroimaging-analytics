e88053895a2ab4604b4f459b9102dd40cdb4d2ac
magponcedeleon <33703522+magponcedeleon@users.noreply.github.com>
2025-08-09 16:41:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -59,14 +59,4 @@ document.addEventListener("DOMContentLoaded", () => {
     mobileMenu.classList.toggle("hidden");
   });
 
-  <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-TJVLZ6GB0H"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-TJVLZ6GB0H');
-  </script>
-
 });
