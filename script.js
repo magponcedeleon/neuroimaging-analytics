b3d479d2c273432b90f6a30c2ce00362d42d52ae
magponcedeleon <33703522+magponcedeleon@users.noreply.github.com>
2025-11-27 16:17:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+  // -----------------------------
+  // Google Analytics
+  // -----------------------------
+  const gaScript = document.createElement("script");
+  gaScript.async = true;
+  gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-TJVLZ6GB0H";
+  document.head.appendChild(gaScript);
+
+  gaScript.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ dataLayer.push(arguments); }
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', 'G-TJVLZ6GB0H');
+  };
 
   // -----------------------------
   // Carousel
